45b793c129d718e06d7bf4d45f7fa8c05421389c
Intelwi <mlk04@wp.pl>
2023-07-25 14:54:35 +0200

    First commit

---
+++ scripts/network.py
@@ -0,0 +1,81 @@
+import torch
+import torch.nn as nn
+
+class DLC(nn.Module):
+    def __init__(self, noutputs, nfeats, nstates, kernels, strides, poolsize, enable_activity_signals=False,
+                 activity_vector_size=4):
+        super(DLC, self).__init__()
+        self.noutputs = noutputs
+        self.nfeats = nfeats
+        self.nstates = nstates
+        self.kernels = kernels
+        self.strides = strides
+        self.poolsize = poolsize
+        self.enable_activity_signals = enable_activity_signals
+        self.activity_vector_size = activity_vector_size
+        self.features = nn.Sequential(
+            nn.Conv2d(in_channels=self.nfeats, out_channels=self.nstates[0], kernel_size=self.kernels[0],
+                      stride=self.strides[0], padding=1),
+            nn.BatchNorm2d(nstates[0]),
+            nn.ReLU(),
+            nn.MaxPool2d(self.poolsize),
+            nn.Conv2d(in_channels=self.nstates[0], out_channels=self.nstates[1], kernel_size=self.kernels[1],
+                      stride=self.strides[1]),
+            nn.BatchNorm2d(self.nstates[1]),
+            nn.ReLU(),
+            nn.MaxPool2d(self.poolsize),
+            nn.Conv2d(in_channels=self.nstates[1], out_channels=self.nstates[2], kernel_size=self.kernels[1],
+                      stride=self.strides[1]),
+            nn.BatchNorm2d(self.nstates[2]),
+            nn.ReLU(),
+            nn.MaxPool2d(self.poolsize),
+        )
+        self.image_classifier = nn.Sequential(
+            nn.Linear(self.nstates[2] * self.kernels[1] * self.kernels[1], self.nstates[3]),
+            nn.ReLU(),
+            nn.Linear(self.nstates[3], self.noutputs),
+        )
+
+        self.linear = nn.Sequential(
+            nn.Linear(self.activity_vector_size, 16),
+            nn.ReLU()
+        )
+
+        self.fc1 = nn.Sequential(
+            nn.Linear(self.nstates[2] * self.kernels[1] * self.kernels[1], 16),
+            nn.ReLU()
+        )
+        self.fc2 = nn.Sequential(
+            nn.Linear(32, 16),
+            nn.ReLU()
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(16, self.noutputs),
+        )
+
+    def forward(self, x):
+        if self.enable_activity_signals and len(x)!=2:
+            raise NotImplementedError
+
+        if self.enable_activity_signals:
+            image = x[0]
+            activity_vector = x[1]
+        else:
+            image = x
+
+        features = self.features(image)
+        features = features.view(features.size(0), self.nstates[2] * self.kernels[1] * self.kernels[1])
+
+        if self.enable_activity_signals:
+            conv = self.fc1(features)
+            ac = self.linear(activity_vector)
+            cat = torch.cat((conv, ac), 1)
+            cat = self.fc2(cat)
+            result = self.classifier(cat)
+        else:
+            result = self.image_classifier(features)
+
+        return result
+
+    def get_params_count(self):
+        return sum(p.numel() for p in self.parameters())
---
+++ scripts/wrapper.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+import rospy
+import numpy as np
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+from collections import deque
+import cv2
+import random
+import torch
+from deep_classifier import DeepClassifier
+
+class DeepBehaviourModelWrapper:
+    def __init__(self):
+        self.camera_topic = '/camera/color/image_raw'
+        
+        self.camera_sub = rospy.Subscriber(self.camera_topic,
+                                            Image,
+                                            self.camera_cb)
+        self.image_pub = rospy.Publisher('test_img', Image, queue_size=10)
+        self.frame_buffer = deque(maxlen=200)
+        self.bridge = CvBridge()
+        self.frame = np.ones((198, 198))
+        self.model = DeepClassifier(input_state_size=1, path='/home/michal/thesis/dl_behaviour_model/results/dlc/checkpoints')
+        self.mean = 127
+        self.std = 77
+
+    def camera_cb(self, msg):
+        cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='mono8')
+        cv_image = cv2.resize(cv_image, (198, 198))
+        self.frame_buffer.append(cv_image)
+        image_message = self.bridge.cv2_to_imgmsg(cv_image)
+        self.image_pub.publish(image_message)
+        
+    def get_similarity(self, frame1, frame2):
+        diff = frame1/255 - frame2/255
+        diff = np.around(diff, 2)
+        nonzero_elem = np.count_nonzero(diff)
+        total = diff.shape[0]*diff.shape[1]
+        
+        return 1.-nonzero_elem/total
+        
+    def filter_frames(self, frames, size=8, threshold=0.8):
+        similarities = []
+
+        for idx in range(0, frames.shape[0]-1):
+            similarity = self.get_similarity(frames[idx], frames[idx+1])
+            similarities.append(similarity)
+
+        similarities = np.array(similarities)
+        dissimilar_ids = np.argwhere(similarities < threshold)
+        dissimilarities = (similarities[dissimilar_ids]).flatten()
+
+        if not list(dissimilar_ids):
+            random_frame_id = random.randint(0, frames.shape[0]-1)
+            random_frame = frames[random_frame_id]
+            frames = np.array([random_frame]*size)
+        
+        else:
+            if len(dissimilar_ids) < size:
+                frames = frames[dissimilar_ids]
+                last_frame = frames[-1]
+                frames = list(frames)
+                [frames.append(last_frame) for id in range(size-len(dissimilar_ids))]
+
+            elif len(dissimilar_ids) == size:
+                frames = frames[dissimilar_ids]
+
+            else:
+                indexes = np.argpartition(dissimilarities, size)[:size]
+                indexes = np.sort(indexes)
+                frames = frames[dissimilar_ids[indexes]]
+        return frames
+            
+    def act(self):
+        frames = self.filter_frames(np.array(self.frame_buffer))
+        frames = (np.asarray(frames, dtype=np.float32)-self.mean)/self.std
+        frames = torch.from_numpy(frames)
+        frames = torch.movedim(frames, 1, 0)
+        activity = torch.from_numpy(np.asarray([1, 0, 1, 0], dtype=np.float32))
+        prediction = self.model.majority_vote((frames, activity))
+        prediction = prediction
+        print(prediction)
+        
+
+
+if __name__ == '__main__':
+    
+    rospy.init_node('deep_behaviour_model_wrapper')
+
+    deep_behaviour_model = DeepBehaviourModelWrapper()
+    
+    rospy.sleep(1.0)
+    
+    try:
+        while not rospy.is_shutdown():
+            deep_behaviour_model.act()
+            rospy.sleep(0.5)
+    except rospy.ROSInterruptException as exc:
+        print('Deep behaviour model wrapper exiting...')
