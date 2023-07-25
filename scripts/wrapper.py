e88250e5865609787a9874ed9cdcc8f17dd108f3
Michal Stolarz <mlk04@wp.pl>
2023-07-25 18:34:52 +0200

    Update wrapper.py

--- scripts/wrapper.py
+++ scripts/wrapper.py
@@ -1,104 +1,103 @@
-#!/usr/bin/env python3
-import rospy
 import numpy as np
-from sensor_msgs.msg import Image
-from cv_bridge import CvBridge
-from collections import deque
-import cv2
-import random
 import torch
-from deep_classifier import DeepClassifier
-import rospkg
+import torch.nn as nn
+import torch.optim as optim
 import os
+from network import DLC
+import config_dlc as dcfg
+import re
+import matplotlib.pyplot as plt
 
-class DeepBehaviourModelWrapper:
-    def __init__(self, path):
-        self.camera_topic = rospy.get_param('~image_topic', '/camera/color/image_raw')
-        
-        self.camera_sub = rospy.Subscriber(self.camera_topic,
-                                            Image,
-                                            self.camera_cb)
-        self.image_pub = rospy.Publisher('test_img', Image, queue_size=10)
-        self.frame_buffer = deque(maxlen=200)
-        self.bridge = CvBridge()
-        self.frame = np.ones((198, 198))
-        self.model = DeepClassifier(input_state_size=1, path=model_path)
-        self.mean = 127
-        self.std = 77
+torch.manual_seed(42)
 
-    def camera_cb(self, msg):
-        cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='mono8')
-        cv_image = cv2.resize(cv_image, (198, 198))
-        self.frame_buffer.append(cv_image)
-        image_message = self.bridge.cv2_to_imgmsg(cv_image)
-        self.image_pub.publish(image_message)
-        
-    def get_similarity(self, frame1, frame2):
-        diff = frame1/255 - frame2/255
-        diff = np.around(diff, 2)
-        nonzero_elem = np.count_nonzero(diff)
-        total = diff.shape[0]*diff.shape[1]
-        
-        return 1.-nonzero_elem/total
-        
-    def filter_frames(self, frames, size=8, threshold=0.8):
-        similarities = []
 
-        for idx in range(0, frames.shape[0]-1):
-            similarity = self.get_similarity(frames[idx], frames[idx+1])
-            similarities.append(similarity)
+class DeepClassifier:
+    def __init__(self, path, cfg=dcfg, validation=False, input_state_size=1):
+        # cpu or cuda
+        self.device = cfg.device  # torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.state_dim = cfg.proc_frame_size  # State dimensionality 84x84.
+        self.state_size_data = cfg.state_size_data
+        self.minibatch_size = cfg.minibatch_size
+        self.learning_rate = cfg.learning_rate
+        self.validation = validation
+        self.epochs_num = cfg.epochs
+        self.epoch_end = self.epochs_num
+        self.epoch_start = 0
+        self.input_state_size = input_state_size
+        self.classes_num = cfg.noutputs
+        self.conf_matrix_labels = cfg.labels
+        checkpoint_path = path
 
-        similarities = np.array(similarities)
-        dissimilar_ids = np.argwhere(similarities < threshold)
-        dissimilarities = (similarities[dissimilar_ids]).flatten()
-
-        if not list(dissimilar_ids):
-            random_frame_id = random.randint(0, frames.shape[0]-1)
-            random_frame = frames[random_frame_id]
-            frames = np.array([random_frame]*size)
-        
+        # model
+        if self.input_state_size == 1:
+            nstates = cfg.nstates
+            nfeats = cfg.nfeats
         else:
-            if len(dissimilar_ids) < size:
-                frames = frames[dissimilar_ids]
-                last_frame = frames[-1]
-                frames = list(frames)
-                [frames.append(last_frame) for id in range(size-len(dissimilar_ids))]
+            raise ValueError("Unaccountable state size")
 
-            elif len(dissimilar_ids) == size:
-                frames = frames[dissimilar_ids]
+        self.model = DLC(noutputs=cfg.noutputs, nfeats=nfeats,
+                         nstates=nstates, kernels=cfg.kernels,
+                         strides=cfg.strides, poolsize=cfg.poolsize,
+                         enable_activity_signals=True)
 
+        checkpoint_name = 'model_epoch'
+        pattern = re.compile(checkpoint_name + '[0-9]+.pt')
+
+        # metrics
+        #self.accuracy = torchmetrics.classification.Accuracy(task='multiclass', average='macro', num_classes=self.classes_num)
+        #self.precision = torchmetrics.classification.Precision(task="multiclass", average='macro', num_classes=self.classes_num)
+        #self.recall = torchmetrics.classification.Recall(task="multiclass", average='macro', num_classes=self.classes_num)
+        #self.f1_score = torchmetrics.classification.F1Score(task="multiclass", average='macro', num_classes=self.classes_num)
+        #self.conf_mat = torchmetrics.classification.ConfusionMatrix(task="multiclass", num_classes=self.classes_num)
+
+        params_count = self.model.get_params_count()
+        print(f"Number of trainable parameters {params_count}")
+
+        #self.optimizer = optim.Adam(self.model.parameters(), self.learning_rate)
+        #self.loss_fuction = nn.CrossEntropyLoss()
+
+        try:
+            if os.path.exists(checkpoint_path):
+                entries = sorted(os.listdir(checkpoint_path), reverse=True)
+                if entries:
+                    numbers = [int(re.findall('\d+', entry)[0]) for entry in entries if pattern.match(entry)]
+                    epoch = max(numbers)
+
+                    self.epoch_end = epoch + self.epochs_num + 1
+                    self.epoch_start = epoch + 1
+
+                    checkpoint = torch.load(os.path.join(checkpoint_path, f"{checkpoint_name}{epoch}.pt"), map_location=torch.device('cpu'))
+                    self.model.load_state_dict(checkpoint['model_state_dict'])
+                    print("MODEL FOUND")
+                else:
+                    print("NO MODEL FOUND")
             else:
-                indexes = np.argpartition(dissimilarities, size)[:size]
-                indexes = np.sort(indexes)
-                frames = frames[dissimilar_ids[indexes]]
-        return frames
-            
-    def act(self):
-        frames = self.filter_frames(np.array(self.frame_buffer))
-        frames = (np.asarray(frames, dtype=np.float32)-self.mean)/self.std
-        frames = torch.from_numpy(frames)
-        frames = torch.movedim(frames, 1, 0)
-        activity = torch.from_numpy(np.asarray([1, 0, 1, 0], dtype=np.float32))
-        prediction = self.model.majority_vote((frames, activity))
-        prediction = prediction
-        print(prediction)
-        
+                print("NO MODEL FOUND")
 
+        except Exception as err:
+            print(repr(err))
 
-if __name__ == '__main__':
-    
-    rospy.init_node('deep_behaviour_model')
-    rospack = rospkg.RosPack()
-    package_path = rospack.get_path('migrave_deep_behaviour_model')
-    model_path = os.path.join(package_path, 'checkpoint')
+    def predict(self, tensor):
+        self.model.eval()
+        images, activity = tensor
 
-    deep_behaviour_model = DeepBehaviourModelWrapper(path=model_path)
-    
-    rospy.sleep(1.0)
-    
-    try:
-        while not rospy.is_shutdown():
-            deep_behaviour_model.act()
-            rospy.sleep(0.5)
-    except rospy.ROSInterruptException as exc:
-        print('Deep behaviour model wrapper exiting...')
+        images, activity = images.to(self.device), activity.to(self.device)
+        full_state = [images, activity]
+
+        with torch.no_grad():
+            action_values = self.model(full_state)
+
+        return torch.argmax(action_values.cpu().detach().clone(), dim=1)
+
+    def majority_vote(self, tensor):
+        images, activity = tensor
+
+        if self.input_state_size != 1:
+            raise 'Function unusable with input state size != 1'
+
+        images = torch.unsqueeze(images[0], dim=1)
+        activity = activity.repeat(images.shape[0], 1)
+        full_state = [images, activity]
+        predictions = self.predict(full_state)
+
+        return np.bincount(predictions).argmax()
